6d36bb5be57c9e3f03251573021448cf612eff01
Kurenairisu <minec323@gmail.com>
2024-07-14 19:23:48 +0800

    js

--- services.php
+++ services.php
@@ -109,13 +109,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 </script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.0/js/bootstrap.bundle.min.js" defer></script>
-<script src="https://unpkg.com/aos@next/dist/aos.js" defer></script>
-<script defer>
-document.addEventListener("DOMContentLoaded", function() {
-    AOS.init();
-});
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+</script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script> <!-- AOS JS -->
+<script>
+AOS.init();
 </script>
 
 </html>
